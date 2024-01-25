ca37f8c29e73a44bd9014761e002cada0e94553e
L1F20BSSE0373 <L1F20BSSE0373@ucp.com>
2024-01-25 13:17:13 +0500

    added all files

--- calculator.cpp
+++ calculator.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Calculator {
-private:
-	double number1;
-	double number2;
 
-public:
-	Calculator(double num1, double num2) {
-		number1 = num1;
-		number2 = num2;
-	}
 
-	double add() {
-		return number1 + number2;
-	}
-
-	double subtract() {
-		return number1 - number2;
-	}
-
-	double multiply() {
-		return number1 * number2;
-	}
-
-	double divide() {
-		if (number2 == 0) {
-			throw "Cannot divide by zero.";
-		}
-		return number1 / number2;
-	}
-};
 
 int main() {
 	Calculator calculator(10, 5);
