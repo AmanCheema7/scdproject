61963b3c83197837692ecb7cbc2e641cb1cfc7c2
L1F20BSSE0373 <L1F20BSSE0373@ucp.com>
2024-01-25 13:10:47 +0500

    added addition and subtraction file

---
+++ add-sub.cpp
@@ -0,0 +1,13 @@
+public:
+	Calculator(double num1, double num2) {
+		number1 = num1;
+		number2 = num2;
+	}
+
+	double add() {
+		return number1 + number2;
+	}
+
+	double subtract() {
+		return number1 - number2;
+	}
